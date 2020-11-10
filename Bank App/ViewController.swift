e6ca631ebd87bd3433b66be1c719949f4becf446
Vasiliy Nikolaev <vasiliy@fitness-kit.ru>
2020-11-10 15:15:50 +0500

    Added currency formatter

--- Bank App/ViewController.swift
+++ Bank App/ViewController.swift
@@ -9,9 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var amountLabel: UILabel!
+    
+    var amount: Double = 10000 {
+        didSet {
+            updateAmountLabel()
+        }
+    }
+    
+    func updateAmountLabel() {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        formatter.currencySymbol = "₽"
+        
+        amountLabel.text = formatter.string(from: NSNumber(value: amount))
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        updateAmountLabel()
+        
     }
 
 
